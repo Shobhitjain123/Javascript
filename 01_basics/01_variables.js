66cf44e83d5850796ba9b2abc99ed409c9e2495d
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-05-25 16:08:24 +0530

    Convertion of data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+let accountName = "SJ"
+var accountPassword = "123456"
+
+accountName = "WA"
+var accountPassword = "1111111111"
+
+console.log(accountPassword)
---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,23 @@
+// let score = "false"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// ------------------------------------------------------
+
+
+
+let isLoggedIn = ""
+
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let booleanLoggenIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanLoggenIn)
+console.log(booleanLoggenIn)
