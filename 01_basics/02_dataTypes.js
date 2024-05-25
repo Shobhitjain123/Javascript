a2c3dfb0c8f7b9d7ecf71e12263bc770bab77694
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-05-25 17:06:05 +0530

    dataTypes and Comparisons

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let id = Symbol('123')
+
+let anotherId = Symbol('123')
+
+console.log(id)
+console.log(anotherId)
+console.log(id == anotherId)
+console.log(id === anotherId)
--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -14,10 +14,40 @@
 
 let isLoggedIn = ""
 
-console.log(typeof isLoggedIn)
-console.log(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(isLoggedIn)
 
 let booleanLoggenIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanLoggenIn)
-console.log(booleanLoggenIn)
+// console.log(typeof booleanLoggenIn)
+// console.log(booleanLoggenIn)
+
+
+
+
+
+
+
+// ------------------------- Operations --------------------------------
+
+let str1 = "Hello "
+let str2 = "Shobhit"
+
+console.log(str1 + str2)
+console.log(str1 - str2)
+
+console.log("1" + 2)
+console.log(typeof ("1" + 2))
+
+console.log(1 + 2 + "3")
+console.log(typeof (1 + 2 + "3"))
+
+console.log("3" + 2 + 1)
+console.log(typeof ("3" + 2 + 1))
+
+
+
+
+
+
+
