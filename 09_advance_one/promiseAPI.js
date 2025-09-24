c7eca2d1cc07f2f7446aa8142f2e3cf911aa5760
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2025-09-24 17:54:08 +0530

    Promise APIs

---
+++ 09_advance_one/promiseAPI.js
@@ -0,0 +1,51 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("P1 is success")
+    }, 3000)
+})
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve("P2 is success")
+        reject("P2 is Fail")
+    }, 1000)
+})
+
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve("P3 is success")
+        reject("P3 is Fail")
+    }, 2000)
+})
+
+Promise.all([p1, p2, p3])
+.then(res => {
+    console.log(res);
+})
+.catch(error => {
+    console.log(error);
+})
+
+Promise.allSettled([p1, p2, p3])
+.then(res => {
+    console.log(res);
+})
+.catch(error => {
+    console.log(error);
+})
+
+Promise.race([p1, p2, p3])
+.then(res => {
+    console.log(res);
+})
+.catch(error => {
+    console.log(error);
+})
+
+Promise.any([p1, p2, p3])
+.then(res => {
+    console.log(res);
+})
+.catch(error => {
+    console.log(error);
+})
